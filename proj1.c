677af44ba55d40e79e6a30f85b7b5703d01d5044
rahals <rahals@mail.gvsu.edu>
2011-10-06 10:06:00 -0400

    finalV

--- proj1.c
+++ proj1.c
@@ -1,13 +1,8 @@
-#include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/time.h>
-#include <time.h>
 #include <sys/wait.h>
-#include <sys/resource.h>
 
 
 typedef int (gvpipe_t) [2];      
@@ -17,38 +12,48 @@ int i,val;
 int z = 0;
 int count = 0;
 int size = 10;
-
 void sChild(int id, gvpipe_t fd[], int N);
 int compare (const void * a, const void * b);
 
 
-
+/**********************************************************************
+* main takes in a number that represents the number of files to sort.
+* takes in the file names ending in .txt and sorts them using pipes. 
+**********************************************************************/
 int main (void) 
 {
     gvpipe_t *pip;
     pid_t who;
     int numFiles,k;
     char input[100];     
-    
-	printf("new2");
-    	fputs("How many sorting files?  Please enter in a number in a power of 2\n", stdout);
+    	
+	printf("************************************************************\n");
+    	fputs("How many sorting files?  \nPlease enter in a number in a power of 2\n> ", stdout);
+
 	fflush(stdout);
 	if (fgets(input, sizeof input, stdin) != NULL) 
 	{
-	      char *newLine = strchr(input, '\n');
-	      if (newLine != NULL)
-		  *newLine = '\0';
+	      char *scanIn = strchr(input, '\n');
+	      if (scanIn != NULL)
+		  *scanIn = '\0';
     	}
 
     numFiles = atoi (input);
     pip = (gvpipe_t *) calloc (numFiles, sizeof(gvpipe_t));
-    printf("Creating pipes!\n");
+    
+		printf("*****************\n");
+    		printf("Creating pipes!\n");
+		printf("*****************\n");
+
     for (k = 0; k < numFiles; k++)
 	{
      		pipe (pip[k]);
 	}
 
-    printf("Creating children!\n");
+		printf("**************************\n");
+    		printf("Creating children by fork!\n");
+		printf("**************************\n");
+    
     for (k = 0; k < numFiles; k++) 
 	{
         	who = fork ();
@@ -63,77 +68,77 @@ int main (void)
   
       
 	
-	  int values[numFiles];
-	  int pipes[numFiles];
-	  int counts[numFiles];
-	  int winner = -1; 
+	int values[numFiles];
+	int pipes[numFiles];
+	int counts[numFiles];
+	int high = -1; 
 	  
-	  //Everything starts out as 0!
-	  for (z = 0; z < numFiles; z++) {
-	      values[z] = 0;
-	      pipes[z] = 1;
-	      counts[z] = 0;
-	  }
+	for (z = 0; z < numFiles; z++) 
+	{
+		values[z] = 0;
+		pipes[z] = 1;
+	      	counts[z] = 0;
+	}
 
 	int pipeNumber = 0;
-	  int t = 0;
-	  count = 0;
-	  while (count < size * numFiles) {
-	    
-	    count++;	    
-        /********************************************************************************************************/
-	      /*This should read in each time*/
-	      for (t = 0; t < numFiles; t++) {
-		if (pipes[t] == 1 && counts[t] < size) {
-		  read(pip[t][READ], &values[t], sizeof(int));
-		  //printf("Read from pipe %d: value %d\n", z, values[t]);
-		  pipes[t] = 0;
-		  counts[t]++;
-		}
-	      }//end of for loop
+	int t = 0;
+	count = 0;
+	while (count < size * numFiles) 
+	{
+		count++;	    
+	      	for (t = 0; t < numFiles; t++) 
+		{
+			if (pipes[t] == 1 && counts[t] < size) 
+			{
+		  		read(pip[t][READ], &values[t], sizeof(int));
+		  		pipes[t] = 0;
+		  		counts[t]++;
+			}
+	      	}
       
-	      pipeNumber = 0;
-	      for (z = 0; z < numFiles-1; z++) {
-		printf("Pipping out...");
-		if (z == 0) {
-		  winner = values[z];
-		  pipeNumber = z;
+	       pipeNumber = 0;
+	       for (z = 0; z < numFiles-1; z++) 
+		{
+			if (z == 0) {
+		  	high = values[z];
+		  	pipeNumber = z;
 		 }
-		  if (winner > values[z+1]) {
-		    winner = values[z+1];
-		    pipeNumber = z+1;
-		  } else if (winner == values[z+1]) {
-		    if (counts[z] < counts[z+1]) {
-		      winner = values[z];
-		      pipeNumber = z;
-		    } else {
-		      winner = values[z+1];
-		      pipeNumber = z+1;
-		    }
+	       if (high > values[z+1]) 
+		{
+			high = values[z+1];
+		    	pipeNumber = z+1;
+   	        } 
+		else if (high == values[z+1]) 
+		{
+			if (counts[z] < counts[z+1]) 
+			{
+		      		high = values[z];
+		      		pipeNumber = z;
+		        } 
+			else {
+		      		high = values[z+1];
+		      		pipeNumber = z+1;
+		    	}
 		  }
 		  
 		  if (counts[z] > size) {
-		      winner = z + 1;
-		      pipeNumber = z + 1;
+		      	high = z + 1;
+		      	pipeNumber = z + 1;
 		  }
 		  if (counts[z+1] > size) {
-		      winner = z;
-		      pipeNumber = z;
+		      	high = z;
+		      	pipeNumber = z;
 		  }
-		}//end of forloop
+		}
 		
-		printf("  %d\n", winner); //This is the lowest number
-		pipes[pipeNumber] = 1; //this sets the pipe to be read again
+		printf("Piping out...  %d\n", high); 
+		pipes[pipeNumber] = 1; 
 	    }
-      
-       /********************************************************************************************************/
-	   
-
 
      /* now wait for all children to stop */
     for (k = 0; k < numFiles; k++) {
-         int stat;
-         wait (&stat);
+         	int stat;
+         	wait (&stat);
      }
    
     free (pip);
@@ -141,20 +146,20 @@ int main (void)
 }
 
 
-/**********************************************************
- * 		Child process
- **********************************************************/
+
+//		Child process
 void sChild(int id, gvpipe_t fd[], int N) {
 
 	char *fileName;
 	char input[100];
 	
-	fputs("Please enter a file name to sort\n", stdout);
+	fputs("Please enter a file name to sort\n> ", stdout);
 	fflush(stdout);
-	if (fgets(input, sizeof input, stdin) != NULL) {
-	      char *newLine = strchr(input, '\n');
-	      if (newLine != NULL)
-		  *newLine = '\0';
+	if (fgets(input, sizeof input, stdin) != NULL) 
+	{
+	      char *scanIn = strchr(input, '\n');
+	      if (scanIn != NULL)
+		  	*scanIn = '\0';
 	}
 
 	fileName = input;
@@ -165,12 +170,13 @@ void sChild(int id, gvpipe_t fd[], int N) {
 	int temp;
 	int arrayOfInt[80];
 	 i=0;
-	
+	printf("printing out file\n");
 	while(fgets(line,80, fr) !=NULL){
 	  
-	  sscanf(line, "%d", &temp);
-	  arrayOfInt[i] = temp;
-	  i++;
+	  	sscanf(line, "%d", &temp);
+	  	arrayOfInt[i] = temp;
+	  	printf("unsorted : %i\n",temp);
+	  	i++;
 	}
 	size=i;
 	
@@ -179,10 +185,11 @@ void sChild(int id, gvpipe_t fd[], int N) {
     
 	qsort (arrayOfInt, size, sizeof(int), compare);
 	
-	while(x < i){
-	    printf("%d\n", arrayOfInt[x]);	
-	      write (fd[id][WRIT], &arrayOfInt[x], sizeof(int));
-	    x++;
+	while(x < i)
+	{
+	    	printf("sorted : %d\n", arrayOfInt[x]);	
+	      	write (fd[id][WRIT], &arrayOfInt[x], sizeof(int));
+	    	x++;
 	}
 	
 	fclose(fr);
@@ -191,7 +198,8 @@ void sChild(int id, gvpipe_t fd[], int N) {
 }
 
 
-//used to compair the 2 ints passed by the pipe
+
+//compaiing 2 integers
 int compare (const void * a, const void * b)
 {
   return ( *(int*)a - *(int*)b );
